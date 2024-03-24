73ed84f4f3df093dbe6011e1642eafff280fa55d
Billionaire_Developer <lawanijeremiah23@gmail.com>
2024-03-25 00:30:17 +0100

    updated

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
-const apikey = 'aced9dfb35a44f9480721f2f2a6d248f'
+const apiKey = 'aced9dfb35a44f9480721f2f2a6d248f';
 
 const blogContainer = document.getElementById("blog-container");
 
-function fetchRandomNews(){
+async function fetchRandomNews(){
     try{
-        const apiUrl = 'https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apikey=${apikey}'
-        const response = await fetch(apiUrl)
+        const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apikey=${apiKey}`
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return data.articles;
     } catch(error){
-        console.error("error fetching file", error)
+        console.error("error fetching random news", error)
         return[]
     }
 }
+
+function displayBlogs(articles){
+    blogContainer.innerHTML = "";
+    articles.forEach((article) => {
+        const blogCard = document.createElement(div);
+        blogCard.classList.add(blog-Card);
+        const img = document.createElement("img");
+        img.src = article.urlToImage;
+        img.alt = article.title;
+        const title = document.createElement("h2");
+        title.textContent = article.title;
+        const description = article.createElement("p");
+        description.textContent = article.description;
+
+
+        blogCard.appendChild("img");
+        blogCard.appendChild("title");
+        blogCard.appendChild("description");
+
+        blogContainer.appendChild(blogCard);
+    });
+}
+
+(async ()=>{
+    try {
+        const articles = await fetchRandomNews();
+        displayBlogs(articles)
+    } catch(error){
+        console.error("error fetching random news", error)
+    }
+})();
